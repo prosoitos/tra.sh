afb40ba75711fc98ec7ac142e8cd2f4e08447457
Marie-Helene Burle <msb2@sfu.ca>
2019-07-19 15:37:26 -0700

    replace f by i in for loop to reduce confusion with dir and file

--- tra_ls.sh
+++ tra_ls.sh
@@ -3,21 +3,21 @@
 files_path=$HOME/.local/share/Trash/files
 info_path=$HOME/.local/share/Trash/info
 
-for f in $HOME/.local/share/Trash/files/*(D)
+for i in $HOME/.local/share/Trash/files/*(D)
 do
-    if [[ -d $f ]] ; then
+    if [[ -d $i ]] ; then
 	dir_or_file="D"
-    elif [[ -f $f ]] ; then
-	dir_or_file="|"
+    elif [[ -f $i ]] ; then
+	dir_or_file=" "
     fi
     # remove $HOME/.local/share/Trash/files/ from f
-	basename=${f#$HOME/.local/share/Trash/files/}
+    basename=${i#$HOME/.local/share/Trash/files/}
 
-	original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
-	deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T/ /') 2> /dev/null
+    original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
+    deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T/ /') 2> /dev/null
 
-	list=$(echo $deletion_time $dir_or_file $original_path \| $basename)
+    list=$(echo $deletion_time $dir_or_file $original_path \| $basename)
 
-	echo $list
+    echo $list
 
 done | sort -r | fzf -i -e +s
--- tra_rs_p.sh
+++ tra_rs_p.sh
@@ -5,15 +5,15 @@ info_path=$HOME/.local/share/Trash/info
 
 file=$(
     # *(D) instead of * to also include the dot files
-    for f in $HOME/.local/share/Trash/files/*(D)
+    for i in $HOME/.local/share/Trash/files/*(D)
     do
-	if [[ -d $f ]] ; then
+	if [[ -d $i ]] ; then
 	    dir_or_file="D"
-	elif [[ -f $f ]] ; then
-	    dir_or_file="|"
+	elif [[ -f $i ]] ; then
+	    dir_or_file=" "
 	fi
 	# remove $HOME/.local/share/Trash/files/ from f
-	basename=${f#$HOME/.local/share/Trash/files/}
+	basename=${i#$HOME/.local/share/Trash/files/}
 
 	# 2> /dev/null so as not to get error messages if the .trashinfo file is missing
 	original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
