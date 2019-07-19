df4b86dfe576db1d51bfd7c9085e37798f013a4d
Marie-Helene Burle <msb2@sfu.ca>
2019-07-19 09:18:24 -0700

    first commit tra_ls.sh

---
+++ tra_ls.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# set -e
+
+files_path=/home/marie/.local/share/Trash/files
+info_path=/home/marie/.local/share/Trash/info
+
+file=$(
+    for f in /home/marie/.local/share/Trash/files/*(D)
+    do
+	# remove /home/marie/.local/share/Trash/files/ from f
+	basename=${f#/home/marie/.local/share/Trash/files/}
+
+	original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
+	deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T/ /') 2> /dev/null
+
+	list=$(echo $deletion_time \| $original_path \| $basename)
+
+	echo $list
+    done | sort -r | fzf -i -e +s
+    )
