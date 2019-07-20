4e22db8bee679409933390c66113247b85ad134d
Marie-Helene Burle <msb2@sfu.ca>
2019-07-19 20:45:41 -0700

    Add ability to only clean files/dir in trash older than a date

---
+++ tra_cl_date.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+files_path=$HOME/.local/share/Trash/files
+info_path=$HOME/.local/share/Trash/info
+
+cutoff=$(date +%F -d "$1 days ago")
+
+for i in $HOME/.local/share/Trash/files/*(D)
+do
+    basename=${i#$HOME/.local/share/Trash/files/}
+
+    deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T.*$//') 2> /dev/null
+
+    if expr $deletion_time "<=" $cutoff >/dev/null; then
+	rm -r $HOME/.local/share/Trash/files/$basename
+	rm -r $HOME/.local/share/Trash/info/$basename.trashinfo
+    fi
+done
