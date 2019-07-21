6f4ca590340640990ea4bd95a5aa0309c40d0275
Marie-Helene Burle <msb2@sfu.ca>
2019-07-21 15:15:14 -0700

    tra_cl.sh: suppress error when the trash is already empty

--- tra_cl.sh
+++ tra_cl.sh
@@ -1,4 +1,12 @@
 #!/bin/zsh
 
+# save stderr in file descriptor 3
+exec 3>&2
+# do not show stderr (prevents 'no matches found' err to be displayed when the trash is empty)
+exec 2> /dev/null
+
 rm -r $HOME/.local/share/Trash/files/*(D)
 rm -r $HOME/.local/share/Trash/info/*(D)
+
+# restore stderr to prevent an exit 1
+exec 2>&3
