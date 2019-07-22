ae7f098a05e3ea4ca9f40093b7f28bfaf9be0a8b
Marie-Helene Burle <msb2@sfu.ca>
2019-07-22 14:21:52 -0700

    update tra_cl_date.sh to make it work on any mount point

--- tra_cl_date.sh
+++ tra_cl_date.sh
@@ -7,26 +7,42 @@
 #     https://github.com/prosoitos
 #     https://twitter.com/MHBurle
 #     msb2@sfu.ca
-#
 
 # This script needs to be called followed by an integer
 # It empties the trash up to (and including) <integer> days ago
+# If <integer> is missing, all files/directories will be deleted from trash except for those trashed today
 
+topdir=$(findmnt -T . -n -o TARGET)
 
-files_path=$HOME/.local/share/Trash/files
-info_path=$HOME/.local/share/Trash/info
+if [[ $topdir = /home ]]
+then
+    trashpath=$HOME/.local/share/Trash/
+else
+    trashpath=$topdir/.Trash
+fi
+
+# save stderr in file descriptor 3
+exec 3>&2
+# do not show stderr (prevents 'cannot remove xxx.trashinfo' error when the the metadata is missing)
+exec 2> /dev/null
+
+files_path=$trashpath/files
+info_path=$trashpath/info
 
 cutoff=$(date +%F -d "$1 days ago")
 
-for i in $HOME/.local/share/Trash/files/*(D)
+for i in $files_path/*(D)
 do
-    basename=${i#$HOME/.local/share/Trash/files/}
+    basename=${i#$files_path/}
 
     deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T.*$//') 2> /dev/null
 
-    if expr $deletion_time "<=" $cutoff >/dev/null
+    if expr "$deletion_time" "<=" "$cutoff" >/dev/null
     then
-	rm -r $HOME/.local/share/Trash/files/$basename
-	rm -r $HOME/.local/share/Trash/info/$basename.trashinfo
+	rm -r $files_path/$basename
+	rm -r $info_path/$basename.trashinfo
     fi
 done
+
+# restore stderr to prevent an exit 1
+exec 2>&3
