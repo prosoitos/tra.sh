03343715a7cb8ee871899774bc8fb0bd64ed7e21
Marie-Helene Burle <msb2@sfu.ca>
2019-07-22 12:28:43 -0700

    make tra_sd.sh complient to the FreeDesktop.org Trash specification

--- tra_sd.sh
+++ tra_sd.sh
@@ -30,5 +30,6 @@ mv $1 $trashpath/files
 
 basename=$(echo $1 | sed -E 's/.*\/(.*)$/\1/')
 
-echo "original_path = $1" > $trashpath/info/$basename.trashinfo
-date +'%F %T' | sed 's/^/deletion_date = /' >> $trashpath/info/$basename.trashinfo
+echo "[Trash Info]" > $trashpath/info/$basename.trashinfo
+echo "Path=$1" >> $trashpath/info/$basename.trashinfo
+date +'%FT%T' | sed 's/^/DeletionDate=/' >> $trashpath/info/$basename.trashinfo
