a3c8836ed8464a7c94aedf0e04e7f0f49eeccbbd
Marie-Helene Burle <msb2@sfu.ca>
2019-07-23 03:19:40 -0700

    add option to select and restore multiple files at once. Closes #4.

--- tra_rs_dir_p.sh
+++ tra_rs_dir_p.sh
@@ -23,50 +23,51 @@ files_path=$trash_path/files
 info_path=$trash_path/info
 dir=$(pwd)
 
-file=$(
-    for i in $files_path/*(D)
+for i in $files_path/*(D)
+do
+    if [[ -d $i ]]
+    then
+	dir_or_file="D"
+    else
+	dir_or_file=" "
+    fi
+
+    # remove $files_path/ from i
+    basename=${i#$files_path/}
+
+    # 2> /dev/null so as not to get error messages if the .trashinfo file is missing
+    original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
+    deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T/ /') 2> /dev/null
+
+    list=$(echo $deletion_time $dir_or_file $basename \| $original_path \| $files_path/$basename )
+
+    echo $list | grep $dir
+
+done | sort -r | fzf -i -e +s -m --preview="source-highlight --failsafe -f esc256 -i {-1}" |
+
+    while read line
+
     do
-	if [[ -d $i ]]
-	then
-	    dir_or_file="D"
-	else
-	    dir_or_file=" "
-	fi
+	# select $basename from selection
+	basename_file=$(echo $line | sed -E 's/.* [D ] (.*) \| .* \| .*/\1/')
 
-	# remove $files_path/ from i
-	basename=${i#$files_path/}
+	original_path_file=$(echo $line | sed -E 's/.* \| (.*) \| .*/\1/')
 
-	# 2> /dev/null so as not to get error messages if the .trashinfo file is missing
-	original_path=$(grep 'Path=' $info_path/$basename.trashinfo | sed 's/Path=//' | sed 's/%20/ /g') 2> /dev/null
-	deletion_time=$(grep 'DeletionDate=' $info_path/$basename.trashinfo | sed 's/DeletionDate=//' | sed 's/T/ /') 2> /dev/null
+	# select only the base part of $original_path (removing basename from it)
+	original_basepath=$(echo $original_path_file | sed -E 's/^(.*)\/.*/\1/')
 
-	list=$(echo $deletion_time $dir_or_file $basename \| $original_path \| $files_path/$basename )
+	# create all parents directory of the deleted files if needed
+	# this won't delete anything if the directories already exists
+	# remove 2> /dev/null to get error messages printed (to debug if the script stops working)
+	# 2> /dev/null allows not to have error message if the script is aborted after fzf
+	mkdir -p $original_basepath 2> /dev/null
 
-	echo $list | grep $dir
+	# exit 1 to stop the script here if it fails so as not to loose the info file
+	# this will add the suffix ~conflict-with-restored-file to any file which has the same name as the restored file
+	# in case I recreated a new file and forgot when restoring the old file
+	# 2> /dev/null: same as previous line of code
+	mv --suffix=~conflict-with-restored-file $files_path/$basename_file $original_path_file 2> /dev/null || exit 1
 
-    done | sort -r | fzf -i -e +s --preview="source-highlight --failsafe -f esc256 -i {-1}"
-    )
-
-# select $basename from selection
-basename_file=$(echo $file | sed -E 's/.* [D ] (.*) \| .* \| .*/\1/')
-
-original_path_file=$(echo $file | sed -E 's/.* \| (.*) \| .*/\1/')
-
-# select only the base part of $original_path (removing basename from it)
-# original_basepath=$(echo $file | sed -E 's/^(.*)\/.* \| .*/\1/')
-original_basepath=$(echo $original_path_file | sed -E 's/^(.*)\/.*/\1/')
-
-# create all parents directory of the deleted files if needed
-# this won't delete anything if the directories already exists
-# remove 2> /dev/null to get error messages printed (to debug if the script stops working)
-# 2> /dev/null allows not to have error message if the script is aborted after fzf
-mkdir -p $original_basepath 2> /dev/null
-
-# exit 1 to stop the script here if it fails so as not to loose the info file
-# this will add the suffix ~conflict-with-restored-file to any file which has the same name as the restored file
-# in case I recreated a new file and forgot when restoring the old file
-# 2> /dev/null: same as previous line of code
-mv --suffix=~conflict-with-restored-file $files_path/$basename_file $original_path_file 2> /dev/null || exit 1
-
-# if all worked, delete info file
-rm $info_path/$basename_file.trashinfo
+	# if all worked, delete info file
+	rm $info_path/$basename_file.trashinfo
+    done
