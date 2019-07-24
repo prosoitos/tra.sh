ad06e54349d940238b45699b975644adbd6d508e
Marie-Helene Burle <msb2@sfu.ca>
2019-07-23 18:07:20 -0700

    better variable names for readability

--- tra_rs.sh
+++ tra_rs.sh
@@ -49,26 +49,27 @@ done | sort -r | fzf -i -e +s -m | sed -E 's/.* [D ] (.* \| .*)/\1/' |
     while read line
 
     do
-	# select $basename from selection
-	basename_file=$(echo $line | sed -E 's/.* \| (.*)/\1/')
+	# select $basename from selected
+	basename_selected=$(echo $line | sed -E 's/.* \| (.*)/\1/')
 
-	original_path_file=$(echo $line | sed -E 's/(.*) \| .*/\1/')
+	# select original_path from selected
+	original_path_selected=$(echo $line | sed -E 's/(.*) \| .*/\1/')
 
 	# select only the base part of $original_path (removing basename from it)
-	original_basepath=$(echo $original_path_file | sed -E 's/^(.*)\/.*/\1/')
+	original_basepath_selected=$(echo $original_path_file | sed -E 's/^(.*)\/.*/\1/')
 
 	# create all parents directory of the deleted files if needed
 	# this won't delete anything if the directories already exists
 	# remove 2> /dev/null to get error messages printed (to debug if the script stops working)
 	# 2> /dev/null allows not to have error message if the script is aborted after fzf
-	mkdir -p $original_basepath 2> /dev/null
+	mkdir -p $original_basepath_selected 2> /dev/null
 
 	# exit 1 to stop the script here if it fails so as not to loose the info file
 	# this will add the suffix ~conflict-with-restored-file to any file which has the same name as the restored file
 	# in case I recreated a new file and forgot when restoring the old file
 	# 2> /dev/null: same as previous line of code
-	mv --suffix=~conflict-with-restored-file $files_path/$basename_file $original_path_file 2> /dev/null || exit 1
+	mv --suffix=~conflict-with-restored-file $files_path/$basename_selected $original_path_selected 2> /dev/null || exit 1
 
 	# if all worked, delete info file
-	rm $info_path/$basename_file.trashinfo
+	rm $info_path/$basename_selected.trashinfo
     done
